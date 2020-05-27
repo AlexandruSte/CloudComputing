7266e9adf232a72966222ba18189c69b33cb4b52
Stefan Alexandru <alex.stefan007@gmail.com>
2020-05-27 09:11:16 +0300

    Project upload

---
+++ Proiect/CloudClient/src/Components/Layout/CustomLayout.js
@@ -0,0 +1,14 @@
+import {Layout} from 'antd';
+import CustomContent from "./CustomContent";
+import CustomHeader from "./CustomHeader";
+
+const CustomLayout = () => {
+    return (
+        < Layout >
+        < CustomHeader / >
+        < CustomContent / >
+        < /Layout>
+)
+}
+
+export default CustomLayout;
